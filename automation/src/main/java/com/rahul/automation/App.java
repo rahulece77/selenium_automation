6e43a234c828b74a6bad017be7e92ca79d806bca
rahulece77 <rahulece77@gmail.com>
2017-12-02 14:46:08 +0530

    adding new automation project

---
+++ automation/src/main/java/com/rahul/automation/App.java
@@ -0,0 +1,17 @@
+package com.rahul.automation;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+        System.out.println( "Hello World!" );
+
+    }
+}
